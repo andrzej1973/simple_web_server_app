b70cf25e4353287d61e6c613c5bc577081b3cb56
andrzejmazur <Ulasek1873!>
2018-02-27 17:18:59 +0100

    run parameters set to default

--- wsgi.py
+++ wsgi.py
@@ -4,14 +4,14 @@ from flask import Flask
 import time
 import datetime
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route("/")
+@application.route("/")
 def hello():
     ts = time.time()
     current_time=datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
     return "[" + current_time + "] GET Request Received by simple_web_server_app.py"
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
